0b0ad616d5d626004713234a69af914243bc1130
Andrej Strudl <lindic.andrej@gmail.com>
2017-10-02 09:56:49 +0200

    added comments to code

--- bowwling.py
+++ bowwling.py
@@ -13,11 +13,11 @@ class Player(object):
         x=random.randint(0,10)
         self.score.append(x)
         remain = 10-x #remaning pins
-        if x == 10:
+        if x == 10: 
             self.framescore.append(x)
             self.scorelist.append([10])
         if x != 10:
-            y=random.randint(0,remain)
+            y=random.randint(0,remain) #now we can only generate from 0 to reamining pins
             self.score.append(y)
             self.framescore.append(x+y)
             self.scorelist.append([x,y])
@@ -39,13 +39,13 @@ class Player(object):
     def showScore(self):
         result=0
         if len(self.scorelist) > 10:
-            scorelist1=self.scorelist[:10]
+            scorelist1=self.scorelist[:10] #we make a copy of list with only 10 frames
         else:
             scorelist1=self.scorelist[:]
         for i in range(2):
-            scorelist1.append([0])
+            scorelist1.append([0]) #we append 0 to it so we avoid indexing problems in the next code
         for val, item in enumerate(self.scorelist):
-            if item == [10]:
+            if item == [10]: #if strike
                 try:
                     value = item[0]+sum(scorelist1[val+1])
            
@@ -54,19 +54,20 @@ class Player(object):
                     result += value
                 except IndexError:
                      pass
-            elif sum(item) == 10:
+            elif sum(item) == 10: #if spare
                 try:
                     value1=sum(item) + scorelist1[val+1][0]
                     result += value1
                 except IndexError:
                     pass
-            else:
+            else: 
                 value2=sum(item)
                 result+=value2
+        #adding score if 10th frame was strike or spare
         if (len(self.framescore)==11 and self.scorelist[8]==[10]):
             result += 2*(self.scorelist[10][0])
         elif (len(self.framescore)==12 and self.scorelist[8]==[10]):
-            result += (2*(self.scorelist[10][0])+self.scorelist[11][0])#enkrat za 8ko enkrat za tazadnjega
+            result += (2*(self.scorelist[10][0])+self.scorelist[11][0])#enkrat za 8 enkrat za zadnjega
         elif (len(self.framescore)==11):
             result += self.scorelist[10][0]
         elif (len(self.framescore)==12):
@@ -80,6 +81,7 @@ eks=Player('Mark')
 i=0
 frame=10
 
+#run for each frame
 while (i<frame):
     iks.newThrow()
     eks.newThrow()
